c90f9034ed5969c8f00d3e4a77a0713a392e4464
kytsuk <kytsuk_ua@mail.ru>
2017-07-19 17:40:46 +0300

    upload file to directory express

---
+++ file-upload/src/app/app.component.ts
@@ -0,0 +1,37 @@
+import {Component, ElementRef} from '@angular/core';
+import {Http} from "@angular/http";
+import "rxjs/add/operator/do";
+import "rxjs/add/operator/map";
+const URL = 'http://localhost:3000';
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Load file on directory';
+  constructor(private http: Http, private el: ElementRef) {}
+
+  upload() {
+    let inputEl: HTMLInputElement = this.el.nativeElement.querySelector('#photo');
+    console.log("iam+ "+inputEl);
+    console.log(inputEl.files);
+    let fileCount: number = inputEl.files.length;
+    let formData = new FormData();
+    if (fileCount > 0) { // a file was selected
+      for (let i = 0; i < fileCount; i++) {
+        formData.append('photo', inputEl.files.item(i));
+        console.log(formData.append('photo', inputEl.files.item(i)));
+      }
+      this.http
+          .post(URL, formData).map((res:any) => res).subscribe(
+          (success) => {
+            console.log(success);
+            alert(success._body);
+          },
+          (error) => alert(error)
+      );
+
+    }
+  }
+}
